3663b43abec07ea9441f9dbab7470c03c8c943a2
Xavier Ordoquy <xordoquy@linovia.com>
2004-07-30 17:49:18 +0000

    The plugin now reads files in ~/.gnomeXX/planner/python
    2004-07-30  Xavier Ordoquy  <xordoquy@linovia.com>
    
    	* data/ui/Makefile.am:
    	* data/ui/python-plugin.ui:
    	* src/planner-python-plugin.c:
    	The plugin now reads files in ~/.gnomeXX/planner/python

--- src/planner-python-plugin.c
+++ src/planner-python-plugin.c
@@ -33,12 +33,15 @@
 #include <gtk/gtkentry.h>
 #include <gtk/gtkstock.h>
 #include <gtk/gtkfilesel.h>
+#include <libgnome/libgnome.h>
 #include <libgnome/gnome-i18n.h>
 #include <libgnomeui/gnome-file-entry.h>
 #include "planner-window.h"
 #include "planner-plugin.h"
 
 #include "pygobject.h"
+
+
 
 typedef struct _PlannerPythonEnv PlannerPythonEnv;
 
@@ -52,25 +55,20 @@ struct _PlannerPythonEnv {
 	PyObject	*globals;
 };
 
-static PlannerPythonEnv *planner_python_env_new (const char *name);
-static void planner_python_env_free             (PlannerPythonEnv *env);
+static PlannerPythonEnv *planner_python_env_new (const gchar       *name);
+static void planner_python_env_free             (PlannerPythonEnv  *env);
 
-static void python_plugin_execute               (BonoboUIComponent *component,
-					         gpointer           user_data,
-					         const gchar       *cname);
+static void python_plugin_execute               (const gchar       *filename,
+						 PlannerWindow     *window,
+						 GHashTable        *scripts);
 
 void        plugin_init                         (PlannerPlugin     *plugin,
 					         PlannerWindow     *main_window);
 void        plugin_exit                         (PlannerPlugin     *plugin);
 
 
-static BonoboUIVerb verbs[] = {
-	BONOBO_UI_VERB ("Python script", python_plugin_execute),
-	BONOBO_UI_VERB_END
-};
-
 static PlannerPythonEnv *
-planner_python_env_new (const char *name)
+planner_python_env_new (const gchar *name)
 {
 	PlannerPythonEnv *env;
 	PyObject         *pDict, *pMain;
@@ -95,97 +93,17 @@ planner_python_env_free (PlannerPythonEnv *env)
 	g_free (env);
 }
 
-static gboolean
-window_file_is_dir (const gchar *file)
-{
-	struct stat sb;
-
-	if ((stat (file, &sb) == 0) && S_ISDIR (sb.st_mode)) {
-		return TRUE;
-	}
-
-	return FALSE;
-}
-
-/*
-static gchar *
-get_last_dir (PlannerWindow *window)
-{
-	PlannerWindowPriv *priv;
-	GConfClient      *gconf_client;
-	gchar            *last_dir;
-	
-	priv = window->priv;
-	
-	gconf_client = planner_application_get_gconf_client ();
-	
-	last_dir = gconf_client_get_string (gconf_client,
-					    GCONF_PATH "/general/last_dir",
-					    NULL);
-	
-	if (last_dir == NULL) {
-		last_dir = g_strdup (g_get_home_dir ());
-	}
-	
-	if (last_dir[strlen (last_dir)] != G_DIR_SEPARATOR) {
-		gchar *tmp;
-		
-		tmp = g_strconcat (last_dir, G_DIR_SEPARATOR_S, NULL);
-		g_free (last_dir);
-		last_dir = tmp;
-	}
-
-	return last_dir;
-}
-*/
-
 static void
-python_plugin_execute (BonoboUIComponent *component,
-		       gpointer           user_data,
-		       const gchar       *cname)
+python_plugin_execute (const gchar   *filename,
+		       PlannerWindow *window,
+		       GHashTable    *scripts)
 {
-	PlannerWindow     *window;
-	PlannerPluginPriv *priv;
 	PlannerPythonEnv  *env;
 	MrpProject        *project;
-
-	const gchar       *filename = NULL;
-	GtkWidget         *file_sel;
-	gint               response;
-	gchar             *last_dir;
 
 	FILE              *fp;
 	PyObject          *pModule;
 	PyObject          *py_widget;
-
-	priv = PLANNER_PLUGIN (user_data)->priv;
-	window = priv->main_window;
-
-	file_sel = gtk_file_selection_new (_("Open a file"));
-
-	/* last_dir = get_last_dir (window); */
-	last_dir = g_strdup_printf ("%s/",g_get_home_dir ());
-	gtk_file_selection_set_filename (GTK_FILE_SELECTION (file_sel), last_dir);
-	g_free (last_dir);
-
-	gtk_window_set_modal (GTK_WINDOW (file_sel), TRUE);
-	
-	gtk_widget_show (file_sel);
-
-	response = gtk_dialog_run (GTK_DIALOG (file_sel));
-
-	if (response == GTK_RESPONSE_OK) {
-		filename = gtk_file_selection_get_filename (GTK_FILE_SELECTION (file_sel));
-		if (window_file_is_dir (filename)) {
-			filename = NULL;
-		}
-	}
-	
-	gtk_widget_destroy (file_sel);
-
-	if (filename == NULL) {
-		return;
-	}
 
 	env = planner_python_env_new (filename);
 
@@ -215,45 +133,51 @@ python_plugin_execute (BonoboUIComponent *component,
 	Py_DECREF (py_widget);
 
 	fp = fopen (filename,"r");
-	if (PyRun_File (fp, (char *) filename, Py_file_input, env->globals, env->globals) == NULL) {
-		PyErr_Print ();
+	if (fp != NULL) {
+		if (PyRun_File (fp, (char *) filename, Py_file_input, env->globals, env->globals) == NULL) {
+			PyErr_Print ();
+		}
+		fclose (fp);
+		g_hash_table_insert (scripts,(gpointer)filename,env);
+	} else {
+		/* FIXME: do the free */
+		g_message("Could not open file: %s",filename);
 	}
-	fclose (fp);
-	/* planner_python_env_free (env); */
-	/* g_hash_table_insert (priv->scripts,(gpointer)filename,env); */
 }
+
 
 G_MODULE_EXPORT void 
 plugin_init (PlannerPlugin *plugin, PlannerWindow *main_window)
 {
 	PlannerPluginPriv *priv;
-	BonoboUIContainer *ui_container;
-	BonoboUIComponent *ui_component;
+	GDir              *dir;
+	gchar             *dirname, *fullfilename;
+	const gchar       *filename;
 	
 	priv = g_new0 (PlannerPluginPriv, 1);
 	plugin->priv = priv;
 	priv->main_window = main_window;
 	priv->scripts = g_hash_table_new (g_str_hash, g_str_equal);
 	
-	ui_container = planner_window_get_ui_container (main_window);
-	ui_component = bonobo_ui_component_new_default ();
-	
-	bonobo_ui_component_set_container (ui_component, 
-					   BONOBO_OBJREF (ui_container),
-					   NULL);
-	bonobo_ui_component_freeze (ui_component, NULL);
-	bonobo_ui_component_add_verb_list_with_data (ui_component, 
-						     verbs,
-						     plugin);
-	bonobo_ui_util_set_ui (ui_component,
-			       DATADIR,
-			       "/planner/ui/python-plugin.ui",
-			       "pythonplugin",
-			       NULL);
-	
-	bonobo_ui_component_thaw (ui_component, NULL);
-
 	Py_Initialize ();
+	
+	/* Look in ~/.gnome2/planner/python/  and run the scripts that we find */
+	dirname = g_build_path(G_DIR_SEPARATOR_S,g_get_home_dir(),GNOME_DOT_GNOME,"planner","python",NULL);
+	dir = g_dir_open(dirname,0,NULL);
+	if (dir == NULL) {
+		g_free(dirname);
+		return;
+	}
+
+	filename = g_dir_read_name(dir);
+	while (filename != NULL) {
+		fullfilename = g_build_filename(dirname,filename,NULL);
+		python_plugin_execute(fullfilename,main_window,priv->scripts);
+		g_free(fullfilename);
+		filename = g_dir_read_name(dir);
+	}
+	g_free(dirname);
+	g_dir_close(dir);
 }
 
 G_MODULE_EXPORT void 
@@ -261,7 +185,7 @@ plugin_exit (PlannerPlugin *plugin)
 {
 	g_message ("%s(%i) FIXME: free the Python plugin priv structure !!!",__FILE__,__LINE__);
 	if (plugin != NULL) {
-		planner_python_env_free (NULL);
+		//planner_python_env_free (NULL);
 	}
 	Py_Finalize ();
 }
